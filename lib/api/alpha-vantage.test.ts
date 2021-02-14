21f2b69dcab6c49b5d1c9e51b6dcd3bcb769b010
yoshikouki <yoshikouki@pepabo.com>
2021-02-14 23:11:54 +0900

    外部APIのテストをモック化した

--- lib/api/alpha-vantage.test.ts
+++ lib/api/alpha-vantage.test.ts
@@ -1,6 +1,30 @@
 import {AlphaVantage} from "./alpha-vantage";
+import axios from "axios";
 
 test('#getTimeSeriesMonthlyAdjusted', async () => {
+  jest.spyOn(axios, 'get').mockResolvedValue(fakeForGetTimeSeriesMonthlyAdjusted)
   const av = new AlphaVantage()
   expect(await av.getTimeSeriesMonthlyAdjusted('IBM')).toBe([])
 })
+
+const fakeForGetTimeSeriesMonthlyAdjusted = {
+  data: {
+    "Meta Data": {
+      "1. Information": "Monthly Adjusted Prices and Volumes",
+      "2. Symbol": "IBM",
+      "3. Last Refreshed": "2021-02-12",
+      "4. Time Zone": "US/Eastern"
+    },
+    "Monthly Adjusted Time Series": {
+      "2021-02-12": {
+        "1. open": "119.9000",
+        "2. high": "123.9767",
+        "3. low": "118.1200",
+        "4. close": "120.8000",
+        "5. adjusted close": "120.8000",
+        "6. volume": "53039869",
+        "7. dividend amount": "1.6300"
+      }
+    }
+  }
+}
--- lib/api/core.test.ts
+++ lib/api/core.test.ts
@@ -1,9 +1,37 @@
 import {requestGet} from "./core";
+import axios from "axios";
 
 test('#requestGet', async () => {
+  jest.spyOn(axios, 'get').mockResolvedValue(fakeRequestGet)
   const res = await requestGet(
     'https://httpbin.org/get',
     {param: 'test'}
     )
   expect(res.args.param).toBe('test')
 })
+
+const fakeRequestGet = {
+  data: {
+    "args": {
+      "param": "test"
+    },
+    "headers": {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "ja,en-US;q=0.9,en;q=0.8",
+      "Dnt": "1",
+      "Host": "httpbin.org",
+      "Sec-Ch-Ua": "\"Chromium\";v=\"88\", \"Google Chrome\";v=\"88\", \";Not A Brand\";v=\"99\"",
+      "Sec-Ch-Ua-Mobile": "?0",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-User": "?1",
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+      "X-Amzn-Trace-Id": "Root=1-6029283b-2ada4f6327a8018d6a22fa9a"
+    },
+    "origin": "133.32.232.41",
+    "url": "https://httpbin.org/get?param=test"
+  }
+}
