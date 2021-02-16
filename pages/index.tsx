95ce59ce39a2543ce88d9d09f153541020b1c22b
yoshikouki <yoshikouki@pepabo.com>
2021-02-17 00:02:36 +0900

    トップページに配当ページへのリンクを配置

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DefaultLayout } from '../layouts/Default'
 import { Typography } from '@material-ui/core'
+import { LinkButton } from '../components/LinkButton'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Typography variant={'h2'}>
         Welcome to <a href="https://dividend-notice.net">Dividend Notice</a>
       </Typography>
+      <LinkButton title={'Dividend'} path={'/dividend'} />
     </DefaultLayout>
   )
 }
