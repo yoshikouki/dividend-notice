8da683d95419ef926b0d65fb04d798c1ed908fbc
yoshikouki <yoshikouki@pepabo.com>
2021-02-18 00:13:37 +0900

    このPRのスコープを基盤作成に変更するので、関係ないコミットを元に戻す

--- lib/api/core.test.ts
+++ lib/api/core.test.ts
@@ -1,9 +1,5 @@
-import { requestGet } from './core'
-import axios from 'axios'
-import { fakeRequestGet } from '../../tests/faker'
+import { returnTrue } from './core'
 
-test('#requestGet', async () => {
-  jest.spyOn(axios, 'get').mockResolvedValue(fakeRequestGet)
-  const res = await requestGet('https://httpbin.org/get', { param: 'test' })
-  expect(res.args.param).toBe('test')
+test('First Test', () => {
+  expect(returnTrue()).toBe(true)
 })
--- lib/api/core.ts
+++ lib/api/core.ts
@@ -1,12 +1,3 @@
-import axios, { AxiosResponse } from 'axios'
-
-export function requestGet(url = 'https://localhost', params = {}) {
-  return axios
-    .get(url, { params: params })
-    .then((res: AxiosResponse) => {
-      return res.data
-    })
-    .catch((err) => {
-      console.error(err)
-    })
+export function returnTrue() {
+  return true
 }
