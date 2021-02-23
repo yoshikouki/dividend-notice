0017b64143652cd374df2d31f6e9e9016d51b80a
yoshikouki <yoshikouki@pepabo.com>
2021-02-23 17:36:03 +0900

    ページ読み込みで Can't resolve 'fs' とエラーが出る。dotenv 系のエラーっぽい
    ```
    ./node_modules/dotenv/lib/main.js:24:0
    Module not found: Can't resolve 'fs'
    null
    
    ```

--- pages/dividend/index.tsx
+++ pages/dividend/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { DefaultLayout } from '../../layouts/Default'
 import { Typography } from '@material-ui/core'
+import {AlphaVantage} from "../../lib/api/alpha-vantage";
 
 const Dividend = () => {
+  const alphaVantage = new AlphaVantage()
   return (
     <DefaultLayout>
       <Typography variant={'h2'}>Dividend</Typography>
+      <p>test: {alphaVantage}</p>
     </DefaultLayout>
   )
 }
