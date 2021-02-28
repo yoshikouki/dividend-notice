b89bfd991ca829903df72178e09bd294933ae7bd
yoshikouki <yoshikouki@pepabo.com>
2021-02-28 22:07:00 +0900

    /dividen/ ページの不要なものを削除

--- pages/dividend/index.tsx
+++ pages/dividend/index.tsx
@@ -1,31 +1,22 @@
 import React from 'react'
 import { DefaultLayout } from '../../layouts/Default'
-import { Typography } from '@material-ui/core'
 import {AlphaVantage, TimeSeriesMonthlyAdjusted} from "../../lib/api/alpha-vantage";
 import {GetServerSideProps} from "next";
 
 interface Props {
-  data: TimeSeriesMonthlyAdjusted
 }
 
 const Dividend = (props: Props) => {
-  const metaData = props.data.metaData
   return (
     <DefaultLayout>
-      <Typography variant={'h2'}>Dividend</Typography>
-      <Typography variant={'h3'}>{metaData.symbol}</Typography>
-      <Typography variant={'subtitle1'}>{metaData.information}</Typography>
-      <Typography variant={'caption'}>Last Refreshd : {metaData.lastRefreshed} ({metaData.timeZone})</Typography>
     </DefaultLayout>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const alphaVantage = new AlphaVantage(process.env.ALPHA_VANTAGE_API_KEY)
-  const data = await alphaVantage.getTimeSeriesMonthlyAdjusted('IBM')
   return {
     props: {
-      data: data
     }
   }
 }
