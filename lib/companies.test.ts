c399791faadfc2d441d3feb90cffaba8d5801932
yoshikouki <yoshikouki@pepabo.com>
2021-03-10 23:02:49 +0900

    TDD するぞ〜〜〜。まずは落ちるテストを書いた

--- lib/companies.test.ts
+++ lib/companies.test.ts
@@ -1,10 +1,12 @@
 import {Company} from "./companies";
-import parser from "csv-parse/lib/sync";
 import {fakeListingStatusForLite} from "../tests/faker";
 import axios from "axios";
 
 test('Company.all', async () => {
   jest.spyOn(axios, 'get').mockResolvedValue(fakeListingStatusForLite)
   const companies = await Company.all()
-  expect(companies[0].symbol).toBe(parser(fakeListingStatusForLite.data)[1][0])
+  const type = companies.map((company) => {
+    return company.assetType
+  })
+  expect(Array.from(new Set(type))).toBe(['Stock'])
 })
