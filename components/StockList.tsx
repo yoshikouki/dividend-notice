ea9f6a7108b1b8091a571ddd19be69eb8483cf54
yoshikouki <yoshikouki@pepabo.com>
2021-03-13 11:39:20 +0900

    銘柄表をコンポーネントに切り出した

---
+++ components/StockList.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+import {DataGrid} from "@material-ui/data-grid";
+import React from "react";
+import {useRouter} from "next/router";
+
+const Wrapper = styled.div`
+  height: 1000px;
+`
+
+interface Props {
+  stocks: any[]
+  type: 'companies' | 'etf'
+}
+
+const StockList = (props: Props) => {
+  const router = useRouter()
+
+  const rows = props.stocks
+  const columns = [
+    { field: 'symbol', headerName: 'Symbol' },
+    { field: 'name', headerName: 'Name', width: 300 },
+    { field: 'exchange', headerName: 'Exchange', width: 120 },
+    { field: 'assetType', headerName: 'Type' },
+    { field: 'ipoDate', headerName: 'IPO Date', width: 120 },
+  ]
+
+  return (
+    <Wrapper>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        onRowClick={async (param) => {
+          let path = `/${props.type}/${param.row.symbol}`
+          await router.push(path)
+        }}
+      />
+    </Wrapper>
+  )
+}
+
+export default StockList
--- pages/companies/index.tsx
+++ pages/companies/index.tsx
@@ -2,42 +2,16 @@ import React from 'react'
 import { DefaultLayout } from '../../layouts/Default'
 import { GetStaticProps } from 'next'
 import { Company } from '../../lib/companies'
-import { DataGrid } from '@material-ui/data-grid'
-import styled from 'styled-components'
-import { useRouter } from 'next/router'
-
-const Wrapper = styled.div`
-  height: 1000px;
-`
+import StockList from "../../components/StockList";
 
 interface Props {
   companies: any[]
 }
 
 const Companies = (props: Props) => {
-  const router = useRouter()
-
-  const rows = props.companies
-  const columns = [
-    { field: 'symbol', headerName: 'Symbol' },
-    { field: 'name', headerName: 'Name', width: 300 },
-    { field: 'exchange', headerName: 'Exchange', width: 120 },
-    { field: 'assetType', headerName: 'Type' },
-    { field: 'ipoDate', headerName: 'IPO Date', width: 120 },
-  ]
-
   return (
     <DefaultLayout>
-      <Wrapper>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          onRowClick={async (param) => {
-            let path = '/companies/' + param.row.symbol
-            await router.push(path)
-          }}
-        />
-      </Wrapper>
+      <StockList stocks={props.companies} type={'etf'} />
     </DefaultLayout>
   )
 }
