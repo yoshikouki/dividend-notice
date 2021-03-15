293a634f5d4a59734aa45755755781b265f6a78d
yoshikouki <yoshikouki@pepabo.com>
2021-03-15 21:32:06 +0900

    使っていない型を削除

--- lib/api/alpha-vantage.ts
+++ lib/api/alpha-vantage.ts
@@ -107,26 +107,6 @@ export interface AlphaVantageData {
   dividendAmount?: number
 }
 
-interface GetTimeSeriesMonthlyAdjustedResponse {
-  'Meta Data': {
-    '1. Information': string
-    '2. Symbol': string
-    '3. Last Refreshed': string
-    '4. Time Zone': string
-  }
-  'Monthly Adjusted Time Series': {
-    string: {
-      '1. open': string
-      '2. high': string
-      '3. low': string
-      '4. close': string
-      '5. adjusted close': string
-      '6. volume': string
-      '7. dividend amount': string
-    }
-  }
-}
-
 export const KeysTableForGetTimeSeriesMonthlyAdjusted = {
   metaData: {
     key: 'Meta Data',
