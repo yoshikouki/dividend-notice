452769e5db36f30419c53bda681b0f23550f1b7f
yoshikouki <yoshikouki@pepabo.com>
2021-03-21 22:54:18 +0900

    バッチは API に作ることにした

---
+++ pages/api/updateAllStocks.ts
@@ -0,0 +1,28 @@
+import {AlphaVantage, ListingStatus} from "../../lib/api/alpha-vantage"
+import parser from "csv-parse/lib/sync";
+import { PrismaClient } from '@prisma/client'
+import { fakeListingStatusForLite } from "../../tests/faker"
+import {NextApiRequest, NextApiResponse} from "next";
+
+interface Data {
+  updatedLength: number
+  updatedRows: ListingStatus
+}
+
+export default function updateAllStocks(req: NextApiRequest, res: NextApiResponse<Data>) {
+  // CSV を取得する
+  // const alphaVantage = new AlphaVantage(process.env.ALPHA_VANTAGE_API_KEY)
+  // const listingStatus = await alphaVantage.getListingStatus()
+  const listingStatus = parser(fakeListingStatusForLite.data)
+  const header = listingStatus.shift()
+
+  // DBに登録
+  const prisma = new PrismaClient()
+  const updatedRows = listingStatus.length
+
+  const response: Data = {
+    updatedLength: updatedRows,
+    updatedRows: listingStatus,
+  }
+  res.status(200).json(response)
+}
