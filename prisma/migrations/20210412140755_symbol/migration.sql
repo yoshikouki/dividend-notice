b34e6de23eeeeaf21fb35677fdf7826c78690c2b
yoshikouki <yoshikouki@pepabo.com>
2021-04-12 23:26:11 +0900

    Revert "DB Stock symbol をユニークにスキーマ変更"
    This reverts commit 52b7f4cc48ee20a701d77daee2735e680d413804.

--- prisma/migrations/20210412140755_symbol/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[symbol]` on the table `Stock` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Stock.symbol_unique" ON "Stock"("symbol");
