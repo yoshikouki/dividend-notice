522e8007d1492cb3ec1f69d30dfbd994d8b7d650
yoshikouki <yoshikouki@pepabo.com>
2021-04-12 16:44:42 +0900

    Stock の最新更新処理の差分取得まで実装した

--- pages/api/updateAllStocks.ts
+++ pages/api/updateAllStocks.ts
@@ -9,35 +9,38 @@ interface Data {
 }
 
 export default async function updateAllStocks(req: NextApiRequest, res: NextApiResponse<Data>) {
-  // CSV を取得する
+  // 最新データをAPIから取得
   const alphaVantage = new AlphaVantage(process.env.ALPHA_VANTAGE_API_KEY)
-  const listingStatus = await alphaVantage.getListingStatus()
+  const listingStatuses = await alphaVantage.getListingStatus()
 
-  // DBに登録
+  // DBの既存データを取得
   const prisma = new PrismaClient()
-  let updatedCount = 0
-  // Promise.all(
-  //   listingStatus.map( async (stock: ListingStatus) => {
-  //     const delistingDate: Date | null = stock.delistingDate === 'null' ? null : new Date(stock.delistingDate!)
-  //     await prisma.stock.create({
-  //       data: {
-  //         status: stock.status!,
-  //         symbol: stock.symbol!,
-  //         name: stock.name!,
-  //         exchange: stock.exchange!,
-  //         assetType: stock.assetType!,
-  //         ipoDate: new Date(stock.ipoDate!),
-  //         delistingDate: delistingDate,
-  //       },
-  //     })
-  //     updatedCount ++
-  //   }),
-  // ).then((values) => {
-  //   console.log(values.length)
-  // })
+  const allStocks = await prisma.stock.findMany()
 
-  const updatedRows = await prisma.stock.findMany()
+  // 最新データと既存データで差分があるデータを抽出
+  // 新規追加される情報もあるので、最新データを元に比較する
+  const diffStocks = listingStatuses.map((ls, index) => {
+    const stockIndex = allStocks.findIndex(stock => ls.symbol === stock.symbol)
+    if (stockIndex === -1) {
+      return ls
+    }
+
+    const row = allStocks.splice(stockIndex, 1)[0]
+    if ( ls.status === row.status
+      && ls.delistingDate === row.delistingDate
+      && ls.exchange === row.exchange
+      && ls.name === row.name
+      && ls.ipoDate === row.ipoDate
+      && ls.assetType === row.assetType) {
+      return ls
+    }
+  })
+
+  // 差分データをDBに登録
+
   const allStocksCount = await prisma.stock.count()
+  const updatedCount = diffStocks.length
+  const updatedRows = diffStocks
 
   const response: Data = {
     allStocksCount: allStocksCount,
